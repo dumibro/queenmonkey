a15fdbaf3a0c95b58634f21b4d28106ce94d0c13
dumibro <92115631+dumibro@users.noreply.github.com>
2021-10-07 23:43:19 +0530

    Update Video.js

--- queenmonkey/Video.js
+++ queenmonkey/Video.js
@@ -3,7 +3,7 @@
 Licensed under the  GPL-3.0 License;
 you may not use this file except in compliance with the License.
 
-Mizuki - king dumiyah
+queenmonkey - king dumiyah
 */
 
 const {MessageType, Presence, MessageOptions} = require('@adiwajshing/baileys');
