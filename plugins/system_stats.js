0fa7b2c26a20826b3317cd1e1ded39cb8590588f
dumibro <92115631+dumibro@users.noreply.github.com>
2021-10-08 00:25:18 +0530

    Update system_stats.js

--- plugins/system_stats.js
+++ plugins/system_stats.js
@@ -1,28 +1,83 @@
-/* Copyright (C) 2020 Yusuf Usta.
-
+/* Copyright (C) sisula welgamage.
+coded by sisula welgamge/mrjoka thejaka
 Licensed under the  GPL-3.0 License;
 you may not use this file except in compliance with the License.
-
-WhatsAsena - Yusuf Usta
+DEVELOPER - MR-JOKA / SISULA WELGAMAGE
 */
 
-const Asena = require('../events');
-const {MessageType} = require('@adiwajshing/baileys');
+const Mizuki = require('../events');
+const {MessageType, MessageOptions, Mimetype} = require('@adiwajshing/baileys');
 const {spawnSync} = require('child_process');
 const Config = require('../config');
+const chalk = require('chalk');
+const axios = require('axios');
 
 const Language = require('../language');
 const Lang = Language.getString('system_stats');
 
-Asena.addCommand({pattern: 'alive', fromMe: true, desc: Lang.ALIVE_DESC}, (async (message, match) => {
-    await message.sendMessage(
-        '```Tanrı Türk\'ü Korusun. 🐺 Asena çalışıyor...```\n\n*Version:* ```'+Config.VERSION+'```\n*Telegram Group:* https://t.me/AsenaSupport\n*Telegram Channel:* https://t.me/WhatsAsena' , MessageType.text
-    );
-}));
 
-Asena.addCommand({pattern: 'sysd', fromMe: true, desc: Lang.SYSD_DESC}, (async (message, match) => {
-    const child = spawnSync('neofetch', ['--stdout']).stdout.toString('utf-8')
-    await message.sendMessage(
-        '```' + child + '```', MessageType.text
-    );
-}));
+if (Config.WORKTYPE == 'private') {
+
+    Mizuki.addCommand({pattern: 'alive', fromMe: true, desc: Lang.ALIVE_DESC}, (async (message, match) => {
+
+        if (Config.ALIVEMSG == 'default') {
+            
+        var aliveimg = await axios.get (Config.ALIVE_LOGO, {responseType: 'arraybuffer'})
+
+        await message.client.sendMessage (message.jid, Buffer.from (aliveimg.data), MessageType.image, {mimetype: Mimetype.png, caption: '```Hey there, I\'m alive now```\n\n_Version:_ ```' + Config.VERSION + '```\n\n_Public Group:_ https://chat.whatsapp.com/ClI0N6kmAkh0dYrU2yOuGj'})
+
+    }
+    else {
+            
+            var image = await axios.get (Config.ALIVE_LOGO, {responseType: 'arraybuffer'})
+       
+        await message.client.sendMessage (message.jid, Buffer.from (image.data), MessageType.image, {mimetype: Mimetype.png, caption: Config.ALIVEMSG + '\n\n*Im Queenmonkey by king dumiyah*' })
+     }
+    }));
+
+    Mizuki.addCommand({pattern: 'sysd', fromMe: true, desc: Lang.SYSD_DESC}, (async (message, match) => {
+
+        if (message.jid === '905524317852-1612300121@g.us') {
+
+            return;
+        }
+
+        const child = spawnSync('neofetch', ['--stdout']).stdout.toString('utf-8')
+        await message.sendMessage(
+            '```' + child + '```', MessageType.text
+        );
+    }));
+}
+else if (Config.WORKTYPE == 'public') {
+
+    Mizuki.addCommand({pattern: 'alive', fromMe: false, desc: Lang.ALIVE_DESC}, (async (message, match) => {
+
+        if (Config.ALIVEMSG == 'default') {
+            
+        var aliveimg = await axios.get (Config.ALIVE_LOGO, {responseType: 'arraybuffer'})
+
+         await message.client.sendMessage (message.jid, Buffer.from (aliveimg.data), MessageType.image, {mimetype: Mimetype.png, caption: '```Hey there, I\'m alive now```\n\n_Version:_ ```' + Config.VERSION + '```\n\n_Public Group:_ https://chat.whatsapp.com/ClI0N6kmAkh0dYrU2yOuGj'})
+
+    }
+    else {
+            
+            var image = await axios.get (Config.ALIVE_LOGO, {responseType: 'arraybuffer'})
+       
+        await message.client.sendMessage (message.jid, Buffer.from (image.data), MessageType.image, {mimetype: Mimetype.png, caption: Config.ALIVEMSG + '\n\n*Im Queenmonkey by king dumiyah*' })
+     }
+    }));
+
+
+    Mizuki.addCommand({pattern: 'sysd', fromMe: false, desc: Lang.SYSD_DESC}, (async (message, match) => {
+
+        if (message.jid === '905524317852-1612300121@g.us') {
+
+            return;
+        }
+
+        const child = spawnSync('neofetch', ['--stdout']).stdout.toString('utf-8')
+        await message.sendMessage(
+            '```' + child + '```', MessageType.text
+        );
+    }));
+} 
