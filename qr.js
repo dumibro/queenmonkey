30e009d00bbd43c7d801aff31a35492de1dc4f63
dumibro <92115631+dumibro@users.noreply.github.com>
2021-10-07 23:25:53 +0530

    Update qr.js

--- qr.js
+++ qr.js
@@ -1,43 +1,55 @@
-/* Copyright (C) 2020 Yusuf Usta.
+/* Copyright (C) 2021 king-dumiyah
 
 Licensed under the  GPL-3.0 License;
 you may not use this file except in compliance with the License.
 
-WhatsAsena - Yusuf Usta
+kingdumiyah 1.0 beta version
 */
 
 const chalk = require('chalk');
-const {WAConnection} = require('@adiwajshing/baileys');
+const {WAConnection, MessageOptions, MessageType} = require('@adiwajshing/baileys');
 const {StringSession} = require('./whatsasena/');
 const fs = require('fs');
 
 async function whatsAsena () {
     const conn = new WAConnection();
     const Session = new StringSession();  
+    conn.version = [2, 2119, 6]
     conn.logger.level = 'warn';
-    conn.regenerateQRIntervalMs = 30000;
+    conn.regenerateQRIntervalMs = 50000;
     
     conn.on('connecting', async () => {
-        console.log(`${chalk.green.bold('Whats')}${chalk.blue.bold('Asena')}
-${chalk.white.italic('AsenaString Kodu Alıcı')}
+        console.log(`${chalk.green.bold('KING')}${chalk.blue.bold('DUMI')}
+${chalk.white.italic('kINGDUMI Code Recipient')}
 
-${chalk.blue.italic('ℹ️  Connecting to Whatsapp... Please wait.')}`);
+${chalk.blue.italic('ℹ️  Connecting to Whatsapp... Please wait.i am kingdumi')}`);
     });
     
 
-    conn.on('open', () => {
+    conn.on('open', async () => {
         var st = Session.createStringSession(conn.base64EncodedAuthInfo());
         console.log(
-            chalk.green.bold('Asena String Kodunuz: '), Session.createStringSession(conn.base64EncodedAuthInfo())
+            chalk.green.bold('Your KINGDUMI String Code: '), Session.createStringSession(conn.base64EncodedAuthInfo())
         );
         
         if (!fs.existsSync('config.env')) {
-            fs.writeFileSync('config.env', `ASENA_SESSION="${st}"`);
+            fs.writeFileSync('config.env', `KINGDUMI_SESSION="${st}"`);
         }
-
-        console.log(
-            chalk.blue.bold('Locale kuruyorsanız node bot.js ile botu başlatabilirsiniz.')
-        );
+        if (conn.user.jid.startsWith('90')) {
+            await conn.sendMessage(conn.user.jid,st, MessageType.text)
+            await conn.sendMessage(conn.user.jid,'*Please Do Not Share This Code With Anyone im KING DUMINDU!*', MessageType.text)
+            console.log(
+                chalk.blue.bold('If you are installing locale, you can start the bot with node bot.js.')
+            );
+        }
+        else {
+            await conn.sendMessage(conn.user.jid,st, MessageType.text)
+            await conn.sendMessage(conn.user.jid,'*Please Do Not Share This Code With Anyone im KING DUMINDU!*', MessageType.text)
+            console.log(
+                chalk.blue.bold('If you are installing locale, you can start the bot with node bot.js')
+            );
+        }
+        
         process.exit(0);
     });
 
